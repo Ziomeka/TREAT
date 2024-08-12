a14ee7e5e2e942f5bb66c6bd64c14b5e441841ad
Agnieszka Ziomek <poziomkaa@gmail.com>
2024-08-12 21:56:32 +0200

    feat: add parsing color file

---
+++ src/getColorsData.ts
@@ -0,0 +1,45 @@
+export interface ColorData {
+    r: number
+    g: number
+    b: number
+    name: string
+}
+
+export default (fileText: string) => {
+    const data = fileText.split('\n');
+    const resultColors: ColorData[] = [];
+    let resultHeader: string[] = [];
+
+    const error = new Error('File format is invalid. Please upload a GIMP palette file (.gpl)')
+    if (data[0] !== 'GIMP Palette') {
+       throw error
+    }
+    const start = data.indexOf('#')
+    if (start < 0 ) {
+        throw error
+    } else {
+        resultHeader = data.slice(0, start + 1);
+        data.slice(start + 1, data.length -1 ).forEach(color => {
+            const colorData = color.trim().replace(/ {2,}/g,' ').split(' ')
+            if (colorData.length !== 4 ) throw error
+            const rgb: ColorData = {
+                r: Number(colorData[0]),
+                g: Number(colorData[1]),
+                b: Number(colorData[2]),
+                name: colorData[3]
+            }
+            if (rgb.r < 256 && rgb.r >= 0 &&
+                rgb.g < 256 && rgb.g >= 0 &&
+                rgb.b < 256 && rgb.b >= 0
+            ) {
+                resultColors.push(rgb)
+            } else {
+                throw error
+            }
+        })
+        return {
+            resultHeader,
+            resultColors
+        }
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import 'virtual:uno.css'
+import type { ColorData } from './getColorsData'
+import getColorsData from './getColorsData'
+
 const fileInput = document.getElementById('fileInput') as HTMLInputElement;
 const colorList = document.getElementById('colorList') as HTMLUListElement;
 
-let colors: string[] = [];
+let resultHeader: string[];
+let resultColors: ColorData[];
 
 fileInput.addEventListener('change', (event) => {
     const file = (event.target as HTMLInputElement).files?.[0];
@@ -11,17 +15,24 @@ fileInput.addEventListener('change', (event) => {
         reader.readAsText(file);
         reader.onload = () => {
             const text = reader.result as string;
-            colors = text.split('\n');
-            renderColors();
+            try {
+              ({resultHeader, resultColors} = getColorsData(text));
+            } catch(e) {
+              alert(e)
+              fileInput.value = ''
+              return;
+            }
+            renderColors(resultColors);
+            console.log(resultHeader)
         };
     }
 });
 
-function renderColors() {
+function renderColors(colors:ColorData[] ) {
   colorList.innerHTML = '';
     colors.forEach((color) => {
         const li = document.createElement('li');
-        li.textContent = color;
+        li.textContent = color.name;
         colorList.appendChild(li);
     });
 }
