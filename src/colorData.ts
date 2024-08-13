a0d26d50f1bfc4687343b480fe3545e36a973e89
Agnieszka Ziomek <poziomkaa@gmail.com>
2024-08-13 10:38:16 +0200

    feat: add stringify color data and save

--- src/colorData.ts
+++ src/colorData.ts
@@ -5,23 +5,23 @@ export interface ColorData {
     name: string
 }
 
-export default (fileText: string) => {
+export const getColorsData = (fileText: string) => {
     const data = fileText.split('\n');
     const resultColors: ColorData[] = [];
     let resultHeader: string[] = [];
 
     const error = new Error('File format is invalid. Please upload a GIMP palette file (.gpl)')
     if (data[0] !== 'GIMP Palette') {
-       throw error
+        throw error
     }
     const start = data.indexOf('#')
-    if (start < 0 ) {
+    if (start < 0) {
         throw error
     } else {
         resultHeader = data.slice(0, start + 1);
-        data.slice(start + 1, data.length -1 ).forEach(color => {
-            const colorData = color.trim().replace(/ {2,}/g,' ').split(' ')
-            if (colorData.length !== 4 ) throw error
+        data.slice(start + 1, data.length - 1).forEach(color => {
+            const colorData = color.trim().replace(/ {2,}/g, ' ').split(' ')
+            if (colorData.length !== 4) throw error
             const rgb: ColorData = {
                 r: Number(colorData[0]),
                 g: Number(colorData[1]),
@@ -43,3 +43,16 @@ export default (fileText: string) => {
         }
     }
 };
+
+const stringifyColor = (color: ColorData) => {
+    const r = color.r.toString().padStart(3, ' ')
+    const g = color.g.toString().padStart(3, ' ')
+    const b = color.b.toString().padStart(3, ' ')
+
+    return `${r} ${g} ${b} ${color.name}`
+}
+
+export const setColorsData = (resultHeader: string[], resultColors: ColorData[]) => {
+    const stringifiedColors = resultColors.map((color) => stringifyColor(color))
+    return `${resultHeader.join('\n')}\n${stringifiedColors.join('\n')}`
+}
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,48 @@
 import 'virtual:uno.css'
-import type { ColorData } from './getColorsData'
-import getColorsData from './getColorsData'
+import type { ColorData } from './colorData'
+import { getColorsData, setColorsData } from './colorData'
 
 const fileInput = document.getElementById('fileInput') as HTMLInputElement;
 const colorList = document.getElementById('colorList') as HTMLUListElement;
+const saveButton = document.getElementById('saveButton') as HTMLUListElement;
 
 let resultHeader: string[];
 let resultColors: ColorData[];
 
 fileInput.addEventListener('change', (event) => {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = () => {
-            const text = reader.result as string;
-            try {
-              ({resultHeader, resultColors} = getColorsData(text));
-            } catch(e) {
-              alert(e)
-              fileInput.value = ''
-              return;
-            }
-            renderColors(resultColors);
-            console.log(resultHeader)
-        };
-    }
+  const file = (event.target as HTMLInputElement).files?.[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+      const text = reader.result as string;
+      try {
+        ({ resultHeader, resultColors } = getColorsData(text));
+      } catch (e) {
+        alert(e)
+        fileInput.value = ''
+        return;
+      }
+      renderColors(resultColors);
+    };
+  }
 });
 
-function renderColors(colors:ColorData[] ) {
+function renderColors(colors: ColorData[]) {
   colorList.innerHTML = '';
-    colors.forEach((color) => {
-        const li = document.createElement('li');
-        li.textContent = color.name;
-        colorList.appendChild(li);
-    });
+  colors.forEach((color) => {
+    const li = document.createElement('li');
+    li.textContent = color.name;
+    colorList.appendChild(li);
+  });
 }
+
+saveButton.addEventListener('click', () => {
+  const blob = new Blob([setColorsData(resultHeader, resultColors)], { type: 'application/x-gimp-palette' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'sorted-data.gpl';
+  a.click();
+  URL.revokeObjectURL(url);
+});
